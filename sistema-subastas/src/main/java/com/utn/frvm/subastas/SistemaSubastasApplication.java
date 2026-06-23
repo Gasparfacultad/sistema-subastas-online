e0b22376d8050d4c5856c7d64f79b5dfeb5e7ee1
GasparGIT <gasparuniversidad@gmail.com>
2026-06-23 18:33:25 -0300

    Inicializacion

---
+++ sistema-subastas/src/main/java/com/utn/frvm/subastas/SistemaSubastasApplication.java
@@ -0,0 +1,13 @@
+package com.utn.frvm.subastas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSubastasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaSubastasApplication.class, args);
+	}
+
+}
---
+++ sistema-subastas/src/test/java/com/utn/frvm/subastas/SistemaSubastasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.frvm.subastas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaSubastasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
